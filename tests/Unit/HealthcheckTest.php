5332e8236848042141c72398d847dcf5d2bee1c2
Mykhailo Sverdlykivskyi <xedelweiss@gmail.com>
2024-03-19 14:54:37 +0200

    Install PHPUnit 10

---
+++ tests/Unit/HealthcheckTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class HealthcheckTest extends TestCase
+{
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+}
