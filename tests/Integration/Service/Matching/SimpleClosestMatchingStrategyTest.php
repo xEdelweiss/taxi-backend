b22ce033395d407179ea4c6e001d5599fe7e6af2
Mykhailo Sverdlykivskyi <xedelweiss@gmail.com>
2024-03-30 23:06:58 +0200

    Implement MatchingStrategy that considers delivery time (lost file)

---
+++ tests/Integration/Service/Matching/SimpleClosestMatchingStrategyTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Tests\Integration\Service\Matching;
+
+use App\Document\TrackingLocation;
+use App\Entity\DriverProfile;
+use App\Entity\Embeddable\Location;
+use App\Entity\User;
+use App\Service\Matching\SimpleClosestMatchingStrategy;
+use Doctrine\ODM\MongoDB\DocumentManager;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use PHPUnit\Framework\Attributes\Test;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class SimpleClosestMatchingStrategyTest extends KernelTestCase
+{
+    private DocumentManager $documentManager;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $kernel = self::bootKernel();
+
+        $this->documentManager = $kernel->getContainer()->get('doctrine_mongodb.odm.default_document_manager');
+        $this->documentManager->createQueryBuilder(TrackingLocation::class)
+            ->remove()
+            ->getQuery()
+            ->execute();
+    }
+
+    #[Test]
+    public function findMatchingDriver(): void
+    {
+        $location1 = new TrackingLocation(1, 46.43045, 30.75475, 'driver'); // 700m
+        $location2 = new TrackingLocation(2, 46.46266, 30.74391, 'driver'); // 4.4km
+        $location3 = new TrackingLocation(3, 46.42804, 30.74694, 'driver'); // 450m
+        $location4 = new TrackingLocation(4, 46.42221, 30.75371, 'driver'); // 600m, but shortest route
+
+        $this->documentManager->persist($location1);
+        $this->documentManager->persist($location2);
+        $this->documentManager->persist($location3);
+        $this->documentManager->persist($location4);
+        $this->documentManager->flush();
+
+        $strategy = new SimpleClosestMatchingStrategy(
+            $this->documentManager,
+            $this->makeEntityManagerMock(),
+        );
+
+        $driverProfile = $strategy->findMatchingDriver(new Location('Some Address', 46.42738, 30.75128));
+        $this->assertInstanceOf(DriverProfile::class, $driverProfile);
+        $this->assertSame(3, $driverProfile->getUser()->getId());
+    }
+
+    private function makeEntityManagerMock(): EntityManagerInterface
+    {
+        $entityManager = $this->createMock(EntityManager::class);
+        $entityManager->expects($this->once())
+            ->method('find')
+            ->willReturnCallback(fn($entity, $id) => $this->makeUser($id));
+
+        return $entityManager;
+    }
+
+    private function makeUser(int $id): User
+    {
+        $user = new User(p($id));
+        $user->setDriverProfile(new DriverProfile($user));
+
+        $reflectionClass = new \ReflectionClass(User::class);
+        $reflectionProperty = $reflectionClass->getProperty('id');
+        $reflectionProperty->setValue($user, $id);
+
+        return $user;
+    }
+}
